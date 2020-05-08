4921df6a4ac0f9ae91b67e5b98924d97b30b6531
Melinda <melinda.konrad@edu.campus02.at>
2020-05-08 12:58:40 +0200

    Updated SimpleDrinkTest.java - Setup established

--- Drinks/src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ Drinks/src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -1,9 +1,28 @@
 package at.fhj.iit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  * Test class for Simpledrink.java
  * Using Junit 5
  */
+@DisplayName("Testing SimpleDrink implementation")
 public class SimpleDrinkTest {
-
+    private Liquid water, cherryLiquer;
+    private SimpleDrink s1,s2;
+    /**
+     * inits simpledrinks and respective liquid for EACH test
+     */
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        water = new Liquid("Wasser", 0.5, 0);
+        cherryLiquer= new Liquid("Cherry liquer", 0.1, 35);
+        s1=new SimpleDrink("Cool water", water);
+        s2= new SimpleDrink("Cherry shot", cherryLiquer);
+    }
 }
