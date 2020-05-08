5dbc923ecdd6a7b23e86be3ff6206d66e903a37b
Melinda <melinda.konrad@edu.campus02.at>
2020-05-08 10:58:25 +0200

    Updated ZeroPercentAlcoholException.java - JavaDoc, Constructor implemented

--- Drinks/src/main/java/at/fhj/iit/ZeroPercentAlcoholException.java
+++ Drinks/src/main/java/at/fhj/iit/ZeroPercentAlcoholException.java
@@ -3,6 +3,15 @@ package at.fhj.iit;
 /**
  * Class inherits from Class Exception
  * Will be thrown in Class Longdrink constructor if Longdrink does not contain alcohol
+ *
+ * @author Melinda Konrad, Lydia Holeckova
  */
-public class ZeroPercentAlcoholException {
+public class ZeroPercentAlcoholException extends Exception{
+    /**
+     * When Exception is thrown, constructor will be called and thus
+     * the super Exception Class will be called
+     */
+    public ZeroPercentAlcoholException(){
+        super();
+    }
 }
