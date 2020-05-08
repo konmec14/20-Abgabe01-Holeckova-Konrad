367449a88040b4372b003166a16dc8a0846b8aac
Melinda <melinda.konrad@edu.campus02.at>
2020-05-08 10:52:59 +0200

    Added unimplemented Class ZeroPercentAlcoholException.java

---
+++ Drinks/src/main/java/at/fhj/iit/ZeroPercentAlcoholException.java
@@ -0,0 +1,8 @@
+package at.fhj.iit;
+
+/**
+ * Class inherits from Class Exception
+ * Will be thrown in Class Longdrink constructor if Longdrink does not contain alcohol
+ */
+public class ZeroPercentAlcoholException {
+}
